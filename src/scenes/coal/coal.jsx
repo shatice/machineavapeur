4f438695a003e7e543196145085e411d08114f89
Arthur Barré <arthur.barre@easy.movie>
2020-06-25 11:03:57 +0200

    remove nm

--- src/scenes/coal/coal.jsx
+++ src/scenes/coal/coal.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import MecaSrc from "../../assets/meca.png";
-import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useState } from "react";
 
+if (typeof window !== `undefined`) {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.core.globals("ScrollTrigger", ScrollTrigger);
+}
+
+const InfiniteScroll = styled.section`
+  height: 300vh;
+  z-index: -1000;
+`;
+
 const CoalWrapper = styled.section`
-  position: relative;
+  position: fixed;
   width: 100vw;
   min-height: 100vh;
   background-color: #d8d6c8;
@@ -65,25 +76,70 @@ let mecaSize = [
 ];
 
 const Coal = () => {
-  const [mecas, setMecas] = useState(mecaSize);
+  const [mecas, setMecas] = useState([]);
+  const [scrollPosition, setSrollPosition] = useState(0);
+  const [tempScrollPosition, setTempSrollPosition] = useState(0);
+
+  const addAMeca = () => {
+    let len = mecas.length;
+    let mecaSave = mecas;
+    mecaSave.push(mecaSize[len]);
+    setMecas(mecaSave);
+  };
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setSrollPosition(position);
+    debugger;
+    setTempSrollPosition(position);
+    debugger;
+    console.log(position);
+    debugger;
+
+    console.log(tempScrollPosition);
+    debugger;
+
+    if (tempScrollPosition > position) console.log("bottom");
+    console.log(scrollPosition);
+    if (position > 100 && position < 120) addAMeca();
+    if (position > 300 && position < 320) addAMeca();
+    if (position > 400 && position < 420) addAMeca();
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   tl.to(container, {
+  //     x: 100,
+  //     duration: 2,
+  //   });
+  // }, []);
 
   return (
-    <CoalWrapper>
-      <MecaContainer>
-        <MecaWrapper>
-          {mecas.map((m) => {
-            return (
-              <MecaElem
-                width={m.width}
-                display={!m.display}
-                src={MecaSrc}
-                alt="meca"
-              />
-            );
-          })}
-        </MecaWrapper>
-      </MecaContainer>
-    </CoalWrapper>
+    <InfiniteScroll>
+      <CoalWrapper>
+        <MecaContainer>
+          <MecaWrapper>
+            {mecas.map((m) => {
+              return (
+                <MecaElem
+                  width={m && m.width}
+                  display={m && !m.display}
+                  src={MecaSrc}
+                  alt="meca"
+                />
+              );
+            })}
+          </MecaWrapper>
+        </MecaContainer>
+      </CoalWrapper>
+    </InfiniteScroll>
   );
 };
 
