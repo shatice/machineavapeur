d7f6cac663c8265f441645c4ca6bbf5ffc388a4b
shatice <sarah.gungor@hetic.net>
2020-06-30 15:55:18 +0200

    [STYLE][AUDIO] import svg audio and place it

--- src/components/atoms/CtaAudio.jsx
+++ src/components/atoms/CtaAudio.jsx
@@ -1,15 +1,24 @@
 import React, { } from "react";
 import styled from "styled-components";
 
+import ctaAduio from '../../assets/svg/cta_audio.svg';
+
 const CtaAudio = () => {
   return (
-    <AudioUI>
-      AUDIO
-    </AudioUI>
+    <AudioUI src={ ctaAduio }/>
   );
 };
-const AudioUI = styled.div `
-  border: solid red; 
+const AudioUI = styled.img `
+  cursor: pointer; 
+  position: absolute; 
+  top: 2rem; 
+  right: 2rem; 
+  width: 2rem; 
+  height: 2rem;
+
+  &:hover {
+    opacity: .2; 
+  }
 `
 
 export default CtaAudio;
