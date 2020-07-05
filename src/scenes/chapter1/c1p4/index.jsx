1482f70ef2abe0eb2118fcf6b4098580e6be390d
shatice <sarah.gungor@hetic.net>
2020-07-06 00:07:32 +0200

    [STYLE] clean style

--- src/scenes/chapter1/c1p4/index.jsx
+++ src/scenes/chapter1/c1p4/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import "../../../styles/styles.scss";
+// import "../../../styles/styles.scss";
 import styled from "styled-components";
 
 /***** COMPONENTS *****/
