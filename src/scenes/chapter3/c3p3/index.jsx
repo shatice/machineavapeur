8c568019445b6c1e200c8aa5d6d4e1d5279c6723
Remi Cakir <remi.cakir@hxperience.com>
2020-07-05 19:48:22 +0200

    [CSS] adapt c3p3 anim

--- src/scenes/chapter3/c3p3/index.jsx
+++ src/scenes/chapter3/c3p3/index.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import bg from "../../../assets/bgC3P3.jpg";
 import nuclearWaste from "../../../assets/nuclear_waste.png";
 import tv from "../../../assets/tv.png";
+import nature from "../../../assets/nature.png";
 
 import Infos from "../../../components/molecules/Infos";
 
@@ -13,12 +14,16 @@ const Chapter3Part3 = () => {
 
     const [nuclearAnim, setNuclearAnim] = useState();
     const [nuclearIsHover, setNuclearIsHover] = useState(false);
-    const [tvAnim, setTvAnim] = useState();
+    const [tvAnim, setTvAnim] = useState({});
     const [tvIsHover, setTvIsHover] = useState(false);
 
-    useEffect(()=> {
-        setNuclearAnim(gsap.to(".nuclearWaste", {y: "-60vh", filter: "grayscale(0)", paused: true}))
-        setTvAnim(gsap.to(".bgC3P3", {filter: "grayscale(0)", paused: true}))
+    useEffect(() => {
+        setNuclearAnim(gsap.to(".nuclearWaste", { y: "-60vh", filter: "grayscale(0)", paused: true }))
+        setTvAnim({
+            bgAnim: gsap.to(".bgC3P3", { filter: "grayscale(0)", duration: 0.4, paused: true }),
+            natureAnim: gsap.to('.nature', { filter: "grayscale(0)", y: -40, delay: 1, paused: true }),
+            tvAnim: gsap.to('.tvContainer', { y: -200, x: -100, scale: 1.1, paused: true })
+        })
     }, [])
 
     const NuclearHover = () => {
@@ -32,9 +37,13 @@ const Chapter3Part3 = () => {
 
     const TvHover = () => {
         if (tvIsHover) {
-            tvAnim.reverse();
+            for (const key in tvAnim) {
+                eval(`tvAnim.${key}.reverse()`);
+            }
         } else {
-            tvAnim.play();
+            for (const key in tvAnim) {
+                eval(`tvAnim.${key}.play()`);
+            }
         }
         setTvIsHover(!tvIsHover);
     };
@@ -42,8 +51,13 @@ const Chapter3Part3 = () => {
     return (<>
         <div className="chapter-container">
             <img src={bg} alt="" className="bg bgC3P3" />
-            <img src={nuclearWaste} alt="" className="nuclearWaste"/>
-            <img src={tv} alt="" className="tv"/>
+            <img src={nuclearWaste} alt="" className="nuclearWaste" />
+            <div className="tvContainer">
+                <div className="relative">
+                    <img src={nature} alt="" className="nature" />
+                    <img src={tv} alt="" className="tv" />
+                </div>
+            </div>
 
             <Infos
                 setIsAnimated={NuclearHover}
