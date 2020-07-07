0c610596011532ff2efd3d93147b9f690e801841
shatice <sarah.gungor@hetic.net>
2020-07-07 11:29:08 +0200

    [CLEAN] resolve warnings for deployment

--- src/scenes/Outro.jsx
+++ src/scenes/Outro.jsx
@@ -2,14 +2,8 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import context from "../store/context";
 
-const Bg = styled.section`
-  width: 100vw;
-  height: 100vh;
-`;
-
 const Outro = () => {
   const { data} = useContext(context);
-  const { outro } = data;
 
   return (
     <Container>
