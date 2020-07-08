e81670ebdd70e2cb822cc78621197e14cbe3c3a3
Remi Cakir <remi.cakir@hxperience.com>
2020-07-08 16:35:02 +0200

    [JS] remove scroll animation between chapter 1 part 1 and part 2

--- src/scenes/chapter1/part2/index.jsx
+++ src/scenes/chapter1/part2/index.jsx
@@ -1,54 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./style.scss";
-import { gsap } from "gsap";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-
 import Mineur from "../../../components/Mineur";
 import Lanterns from "../../../components/Lanterns";
 
 import mineurBG from "../../../assets/img/chap_1/part_2/mineurs_descente.jpg";
 
-gsap.registerPlugin(ScrollToPlugin);
 
-const Chapter1Page2 = ({ data: { parts } = {} }) => {
-  let ref = useRef([]);
-  const currentRef = ref.current;
-
-  useEffect(() => {
-    currentRef["container"].addEventListener("wheel", (e) => {
-      const delta = Math.sign(e.deltaY);
-      if (delta === -1) {
-        gsap.to(".portrait", { rotate: 20, opacity: 1, duration: 1 });
-        gsap.to(".portrait", {
-          duration: 1,
-          delay: 1,
-          yPercent: 0,
-          ease: "exp",
-        });
-        gsap.to(window, {
-          delay: 1.8,
-          duration: 1,
-          scrollTo: ".container",
-          ease: "none",
-        });
-        gsap.to(".portrait", {
-          filter: "grayscale(1)",
-          rotate: 0,
-          duration: 1,
-          ease: "bounce",
-          delay: 2.8,
-        });
-      }
-    });
-  });
+const Chapter1Part2 = ({ data: { parts } = {} }) => {
 
   return (
     <div>
       <div
         className="page2__container"
-        ref={(element) => {
-          currentRef["container"] = element;
-        }}
       >
         <img className="minorBg" src={mineurBG} alt="" />
         <Mineur partData={parts} />
@@ -58,4 +21,4 @@ const Chapter1Page2 = ({ data: { parts } = {} }) => {
   );
 };
 
-export default Chapter1Page2;
+export default Chapter1Part2;
