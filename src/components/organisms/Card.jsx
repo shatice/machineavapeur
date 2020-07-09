75d9b1cf633f9b5e0ff8444e4210b4d03cad8479
hlnmargary <margary.helene@hetic.net>
2020-07-09 19:02:25 +0200

    changed card style

--- src/components/organisms/Card.jsx
+++ src/components/organisms/Card.jsx
@@ -4,13 +4,13 @@ import styled from "styled-components";
 const Card = ({ title, content, topCard, bottomCard, rightCard, leftCard }) => {
   return (
     <Container
-    topCard={ topCard }
-    bottomCard={ bottomCard }
-    rightCard={ rightCard }
-    leftCard={ leftCard }
+      topCard={topCard}
+      bottomCard={bottomCard}
+      rightCard={rightCard}
+      leftCard={leftCard}
     >
-      { title && <Title>{ title }</Title> }
-      <p>{ content }</p>
+      {title && <Title>{title}</Title>}
+      <p>{content}</p>
     </Container>
   );
 };
@@ -21,21 +21,23 @@ const Container = styled.article`
   bottom: ${({ bottomCard }) => bottomCard}%;
   right: ${({ rightCard }) => rightCard}%;
   left: ${({ leftCard }) => leftCard}%;
-  width: 35vw; 
+  width: 40vw; 
   padding: 2rem; 
   background: linear-gradient(3deg, rgba(31,31,33,0.3071603641456583) 0%, rgba(31,31,33,0.7161239495798319) 34%, rgba(31,31,33,1) 100%);
   box-shadow: 1px 2px 10px black;
-  line-height: 1.5;
+  line-height: 1.2;
   animation: fadeIn .5s ease-out;
-
+  border-radius: 2px;
   p {
     font-size: .875rem;
   }
+
+  &::
 `;
 
 const Title = styled.h4`
-  margin-bottom: 1rem; 
-  font-size: .75rem;
+  margin-bottom: 0.4rem; 
+  font-size: 2rem;
   text-transform: uppercase;
 `;
 
